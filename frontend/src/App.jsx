1ec94623e4dd4aa6a4dc81ea91eb1ed4c6ad0902
bitbard3 <ansharora3839@gmail.com>
2024-01-12 18:28:45 +0530

    Configure bootstrap variables and Add fontawesome icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./styles/bootstrap.scss";
+import "./styles/style.scss";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
 function App() {
   return <div></div>;
 }
