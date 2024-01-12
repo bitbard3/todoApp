87e8ee262f8870c6b29babbd497e0bcc088e37de
bitbard3 <ansharora3839@gmail.com>
2024-01-12 19:10:47 +0530

    revert accidental deletion

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import "./styles/bootstrap.scss";
+import "./styles/style.scss";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+function App() {
+  return <div></div>;
+}
+
+export default App;
