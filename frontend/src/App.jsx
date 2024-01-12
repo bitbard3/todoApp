cf587b845bac1999aa80dc4dbfa44d75bf0e282d
bitbard3 <ansharora3839@gmail.com>
2024-01-12 19:01:56 +0530

    Add custom fonts

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import "./styles/bootstrap.scss";
-import "./styles/style.scss";
-import "../node_modules/font-awesome/css/font-awesome.min.css";
-function App() {
-  return <div></div>;
-}
-
-export default App;
