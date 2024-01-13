2b0fc3cf34c427b0c2567d6efbf300a22b2f0ae6
bitbard3 <ansharora3839@gmail.com>
2024-01-14 03:10:18 +0530

    Add cors

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const { authRoutes } = require('./auth/routes')
 const { todoRoutes } = require('./app/routes')
+const cors = require('cors')
 
-
+app.use(cros())
 app.use(express.json())
 
 app.use(authRoutes)
