192a0df70f173ee1708ba8ba55e7691221d8b77f
bitbard3 <ansharora3839@gmail.com>
2024-01-15 02:51:42 +0530

    add home animation

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <div className="bg-secondary vh-100 scrolldown">
+        <div className="d-flex justify-content-center align-items-center h-100">
+          <p className="strike display-2 fw-bold text-primary">todo</p>
+        </div>
+      </div>
+    </div>
+  );
+};
