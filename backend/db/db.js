78c263861d3bedc29294ef6a3612e1b415602888
bitbard3 <ansharora3839@gmail.com>
2024-01-18 12:00:48 +0530

    Edit db url

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://ansharora3839:g68W8vbEth9POW2M@cluster0.qrpua6t.mongodb.net/todo")
+mongoose.connect("mongodb+srv://ansh:hLHXHqzXDbs07a5c@cluster0.33yhny1.mongodb.net/todo")
 
 
 const userSchema = mongoose.Schema({
