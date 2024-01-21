fa171fd507f04eefaeb7dec6abbf15e198f23059
bitbard3 <ansharora3839@gmail.com>
2024-01-21 16:50:48 +0530

    deploy

--- frontend/src/validators/formValidate.js
+++ frontend/src/validators/formValidate.js
@@ -1,4 +1,4 @@
-import z from "zod"
+import { z } from "zod"
 const usernameSchema = z.string({
     required_error: "Username is required",
 }).refine(value => value.length >= 3 && value.length <= 20, {
